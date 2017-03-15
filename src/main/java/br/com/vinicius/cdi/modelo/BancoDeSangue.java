01db19fc867bfbde6810685dfad0f695dbaa5387
JavaRubyNode <viniciusjavaruby@gmail.com>
2017-03-14 23:53:11 -0300

    colocado os atributos a serem mapeados

--- src/main/java/br/com/vinicius/cdi/modelo/BancoDeSangue.java
+++ src/main/java/br/com/vinicius/cdi/modelo/BancoDeSangue.java
@@ -1,13 +1,56 @@
 package br.com.vinicius.cdi.modelo;
 
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import br.com.vinicius.cdi.generic.BaseEntity;
 
 public class BancoDeSangue extends BaseEntity<Long>{
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private Endereco Endereco;
+	private String telefone;
+	private String email;
+	private Integer versionBancoDeSangue; 
+	
+	
+	
 	@Override
-	public Long getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Integer getVersion() {return getVersionBancoDeSangue();}
+
+	@Override
+	public Long getId() {return getId();}
+
+	public String getNome() {return nome;}
+
+	public void setNome(String nome) {this.nome = nome;}
+
+	public Endereco getEndereco() {return Endereco;}
+
+	public void setEndereco(Endereco endereco) {Endereco = endereco;}
+
+	public String getTelefone() {return telefone;}
+
+	public void setTelefone(String telefone) {this.telefone = telefone;}
+
+	public String getEmail() {return email;}
+
+	public void setEmail(String email) {this.email = email;}
+
+	public Integer getVersionBancoDeSangue() {return versionBancoDeSangue;}
+
+	public void setVersionBancoDeSangue(Integer versionBancoDeSangue) {this.versionBancoDeSangue = versionBancoDeSangue;}
+
+
+
+	
+	
+	
 
 }
