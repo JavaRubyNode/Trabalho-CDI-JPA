cd9b6a829c2b740b24612c37c27e6f671a9610b4
MGO108-TI-14937\ti02 <leandroz3ta@gmail.com>
2017-03-15 10:39:25 -0300

    Realizado mapeamento de classe Autor: Leandro Tilaska

--- src/main/java/br/com/vinicius/cdi/modelo/BancoDeSangue.java
+++ src/main/java/br/com/vinicius/cdi/modelo/BancoDeSangue.java
@@ -2,21 +2,37 @@ package br.com.vinicius.cdi.modelo;
 
 
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import br.com.vinicius.cdi.generic.BaseEntity;
 
+@Table(name = "Tb_bancos_de_sangue")
+@Entity
 public class BancoDeSangue extends BaseEntity<Long>{
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "id", unique = true, nullable = false)
 	private Long id;
+	
+	@Column(name = "banco", nullable = false, length = 100)
 	private String nome;
+	
+	@Column(name = "endereco", nullable = false, length = 100)
 	private Endereco Endereco;
+	
+	@Column(name = "telefone", nullable = false, length = 15)
 	private String telefone;
+	
+	@Column(name = "email", nullable = false, length = 20)
 	private String email;
+	
+	@Column(name = "versaoDoBanco", nullable = false, length = 10)
 	private Integer versionBancoDeSangue; 
 	
 	
