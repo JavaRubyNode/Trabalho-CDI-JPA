79b2039ef5fa40eec35b22bc4ca9b57ebad94d58
JavaRubyNode <viniciusjavaruby@gmail.com>
2017-03-15 23:35:13 -0300

    foi construindo os metodos do crud da Entidade

--- src/main/java/br/com/vinicius/cdi/services/BancoDeSangueDao.java
+++ src/main/java/br/com/vinicius/cdi/services/BancoDeSangueDao.java
@@ -1,12 +1,12 @@
 package br.com.vinicius.cdi.services;
 
 import java.util.List;
-
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-
+import javax.persistence.TypedQuery;
 import br.com.vinicius.cdi.interfaces.DAO;
 import br.com.vinicius.cdi.modelo.BancoDeSangue;
+
 
 public class BancoDeSangueDao implements DAO<BancoDeSangue> {
 
@@ -24,25 +24,37 @@ public class BancoDeSangueDao implements DAO<BancoDeSangue> {
 
 	@Override
 	public void altualizar(BancoDeSangue t) {
+		Integer version = t.getVersionBancoDeSangue();
+		BancoDeSangue temp = t;
+		if(temp.getVersionBancoDeSangue() !=version){
+			em.merge(t);
+		}else{
+			salvar(temp);
+		}
 		
 		
 	}
 
 	@Override
 	public void removerPorId(Long id) {
-		// TODO Auto-generated method stub
+		em.getTransaction().begin();
+		BancoDeSangue banco = em.find(BancoDeSangue.class, id);
+		em.remove(banco);
+		em.getTransaction().commit();
 		
 	}
 
 	@Override
 	public List<BancoDeSangue> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<BancoDeSangue> query= em.createQuery("select bs from BancoDeSangue bs  ",BancoDeSangue.class );
+		List<BancoDeSangue> listaRetornoBancoDeSangues = query.getResultList();
+		return listaRetornoBancoDeSangues;
 	}
 
 	@Override
-	public void checarExistencia(BancoDeSangue t) {
-		// TODO Auto-generated method stub
+	public BancoDeSangue checarExistencia(Long id) {
+		BancoDeSangue banco = em.find(BancoDeSangue.class, id);
+		return banco;
 		
 	}
 
